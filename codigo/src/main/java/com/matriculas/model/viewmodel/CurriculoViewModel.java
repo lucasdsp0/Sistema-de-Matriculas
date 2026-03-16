4738745d61ae39ae3da6dbecfd1a02d6dce15e70
yan araujo <saresende555@gmail.com>
2026-03-16 11:44:25 -0300

    Revert "atualizando erro de rota"
    This reverts commit 0c0962b5aa15b4beb305ed23015cb86f0b77dee1.

--- codigo/src/main/java/com/matriculas/model/viewmodel/CurriculoViewModel.java
+++ codigo/src/main/java/com/matriculas/model/viewmodel/CurriculoViewModel.java
@@ -2,14 +2,12 @@ package com.matriculas.model.viewmodel;
 
 public class CurriculoViewModel {
 
-    private Long id;
     private String semestre;
     private String dataInicioFormatted;
     private String dataFimFormatted;
     private int disciplinasCount;
 
-    public CurriculoViewModel(Long id, String semestre, String dataInicioFormatted, String dataFimFormatted, int disciplinasCount) {
-        this.id = id;
+    public CurriculoViewModel(String semestre, String dataInicioFormatted, String dataFimFormatted, int disciplinasCount) {
         this.semestre = semestre;
         this.dataInicioFormatted = dataInicioFormatted;
         this.dataFimFormatted = dataFimFormatted;
@@ -17,10 +15,6 @@ public class CurriculoViewModel {
     }
 
     // Getters
-    public Long getId() {
-        return id;
-    }
-
     public String getSemestre() {
         return semestre;
     }
